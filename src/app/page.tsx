336c3d037b68cffbbfd9843e3bee0b94f81d474e
ridhorz <idhoo.rz@gmail.com>
2024-11-14 13:11:10 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,6 +112,7 @@ export default function Home() {
                   "PHP",
                   "Next.js",
                   "Remix",
+                  "Svelte",
                   "PostgreSQL",
                   "MySQL",
                   "Prisma",
